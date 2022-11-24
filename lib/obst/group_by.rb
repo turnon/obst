5b145f8d1f403c2a6f16147b9516af3ed5694419
ken <block24block@gmail.com>
2022-11-24 12:38:57 +0800

    extract method `each` to seperate module

--- lib/obst/group_by.rb
+++ lib/obst/group_by.rb
@@ -3,11 +3,33 @@ require "time"
 
 module Obst
   module GroupBy
-    class Day
+    module Enum
       include Enumerable
 
+      def each(&block)
+        current_time = @timeline.next
+        record = @log.next
+
+        loop do
+          break unless record
+
+          if record.time == current_time
+            block.call(record)
+            record = @log.next
+          else
+            block.call(PackLog::Record.new(current_time, {}))
+          end
+
+          current_time = @timeline.next
+        end
+      end
+    end
+
+    class Day
+      include Enum
+
       def initialize(**opts)
-        @days = Enumerator.new do |y|
+        @timeline = Enumerator.new do |y|
           curr = Time.now
           one_day = 86400
           loop do
@@ -19,24 +41,6 @@ module Obst
         @log = PackLog.new(**opts) do |committed_at|
           Time.parse(committed_at).strftime('%F')
         end.to_enum
-      end
-
-      def each(&block)
-        day_curr = @days.next
-        record = @log.next
-
-        loop do
-          break unless record
-
-          if record.time == day_curr
-            block.call(record)
-            record = @log.next
-          else
-            block.call(PackLog::Record.new(day_curr, {}))
-          end
-
-          day_curr = @days.next
-        end
       end
     end
   end
