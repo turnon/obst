41ed519b38fe92d9f6e07b9ba41438556dd59578
ken <block24block@gmail.com>
2022-11-26 16:37:58 +0800

    daily gauge

--- lib/obst.rb
+++ lib/obst.rb
@@ -2,7 +2,7 @@ require "obst/version"
 require "obst/git_log"
 require "obst/group_by_days"
 require "obst/touched_files"
-require "obst/daily_increment"
+require "obst/daily_gauge"
 
 module Obst
 end
