1b30dae54fc2f5dc9b49feb8d6d012f75c487abb
ken <block24block@gmail.com>
2022-11-27 00:42:10 +0800

    daily change chart

--- lib/obst/chart.rb
+++ lib/obst/chart.rb
@@ -2,7 +2,7 @@ module Obst
   class Chart
     class DailyCount
       def initialize(**opts)
-        @daily_gauge = Obst::DailyGauge.new(C: opts[:C], days: 1).lazy
+        @daily_gauge = Obst::DailyGauge.new(C: opts[:C]).lazy
       end
 
       def to_s
@@ -15,15 +15,54 @@ module Obst
         <<~EOF
         # Daily count
 
-        - current: #{data[0]}
+        ```chart
+        type: line
+        width: 98%
+        legendPosition: bottom
+        labels: #{labels.reverse!}
+        series:
+          - title: current #{data[0]}
+            data: #{data.reverse!}
+        ```
+        EOF
+      end
+    end
+
+    class DailyChange
+      def initialize(**opts)
+        @daily = Obst::GroupByDays.new(C: opts[:C]).lazy
+      end
+
+      def to_s
+        labels = []
+        datas = {a: [], m: [], d: [], nil => []}
+
+        @daily.take(28 * 3).each do |record|
+          labels << record.date_wday
+          datas.each_value{ |data| data << 0 }
+
+          record.file_changes.each_value do |changes|
+            datas[changes.final][-1] += 1
+          end
+        end
+
+        <<~EOF
+        # Daily change
 
         ```chart
         type: line
         width: 98%
-        legend: false
+        legendPosition: bottom
         labels: #{labels.reverse!}
         series:
-          - data: #{data.reverse!}
+          - title: del
+            data: #{datas[:d].reverse!}
+          - title: nil
+            data: #{datas[nil].reverse!}
+          - title: mod
+            data: #{datas[:m].reverse!}
+          - title: new
+            data: #{datas[:a].reverse!}
         ```
         EOF
       end
