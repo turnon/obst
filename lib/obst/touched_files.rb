2a63e19f54a7e75f8e1b0bd948d8766257ddda73
ken <block24block@gmail.com>
2022-11-27 12:37:33 +0800

    under one header

--- lib/obst/touched_files.rb
+++ lib/obst/touched_files.rb
@@ -4,23 +4,21 @@ module Obst
   class TouchedFiles
     def initialize(**opts)
       @path = opts[:C]
-      @buffer = []
+      @buffer = ["# Touch files in periods\n"]
     end
 
     def to_s
       last_7_days
-      @buffer << ''
       last_4_weeks_without_last_7_days
-      @buffer << ''
       last_3_months_without_last_4_weeks
       @buffer.join("\n")
     end
 
     def last_7_days
-      @buffer << "# Last 7 days\n"
+      @buffer << "- Last 7 days"
 
       GroupByDays.new(C: @path).take(7).each do |record|
-        @buffer << "- #{record.date_wday} (#{record.file_changes.count})"
+        @buffer << "\t- #{record.date_wday} (#{record.file_changes.count})"
         list_files(record)
       end
     end
@@ -28,10 +26,10 @@ module Obst
     def last_4_weeks_without_last_7_days
       before = (Time.now - (60 * 60 * 24 * 7)).strftime('%FT23:59:59')
 
-      @buffer << "# 3 weeks earlier\n"
+      @buffer << "- 1 week ago"
 
-      GroupByDays.new(C: @path, before: before, days: 7).take(3).each do |record|
-        @buffer << "- #{record.time} (#{record.file_changes.count})"
+      GroupByDays.new(C: @path, before: before, days: 7).take(3).each_with_index do |record, i|
+        @buffer << "\t- #{record.time} is #{1+i}.week#{suffix_s(i)}.ago (#{record.file_changes.count})"
         list_files(record)
       end
     end
@@ -39,10 +37,10 @@ module Obst
     def last_3_months_without_last_4_weeks
       before = (Time.now - (60 * 60 * 24 * 28)).strftime('%FT23:59:59')
 
-      @buffer << "# 1 month earlier\n"
+      @buffer << "- 1 month ago"
 
-      GroupByDays.new(C: @path, before: before, days: 28).take(2).each do |record|
-        @buffer << "- #{record.time} (#{record.file_changes.count})"
+      GroupByDays.new(C: @path, before: before, days: 28).take(2).each_with_index do |record, i|
+        @buffer << "\t- #{record.time} is #{1+i}.month#{suffix_s(i)}.ago (#{record.file_changes.count})"
         list_files(record)
       end
     end
@@ -60,12 +58,16 @@ module Obst
         files = group_by_final_status[short]
         next if files.empty?
         inline_str = inline(files)
-        @buffer << "\t- <font color='#{color}'>#{long} #{files.count}:</font> #{inline_str}"
+        @buffer << "\t\t- <font color='#{color}'>#{long} #{files.count}:</font> #{inline_str}"
       end
     end
 
     def inline(files)
       files.sort!.map{ |name| "[[#{name}]]" }.join(' / ')
+    end
+
+    def suffix_s(i)
+      i == 0 ? '' : 's'
     end
   end
 end
